444f5eb5adb017af397d19e32b463c1734f46cbd
Your Name <you@example.com>
2020-09-10 08:28:31 +0700

    create orm on models

---
+++ models/orm/orm.go
@@ -0,0 +1,15 @@
+package orm
+
+import(
+	"net/http"
+	"log"
+	"io/ioutil"
+	"encoding/json"
+)
+
+func InsertData(w http.ResponseWriter, r *http.Request){
+
+}
+func UpdateData(w http.ResponseWriter, r *http.Request){
+	
+}

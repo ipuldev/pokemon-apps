c2d36b8c461e6084087b681fbe23bbe89946499d
Your Name <you@example.com>
2020-09-11 08:46:25 +0700

    update sitik

--- models/orm/orm.go
+++ models/orm/orm.go
@@ -5,11 +5,42 @@ import(
 	"log"
 	"io/ioutil"
 	"encoding/json"
+	"database/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type DBConn struct{
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Power int `json:"power"`
+	Unique string `json:"unique"`
+}
+// Database Connect
+func dbconn()(db *sql.DB){
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "root"
+	dbName := "pokemon"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@"+dbName)
+	err != nil{
+		log.Println(err.Error())
+	}
+	return db
+}
+
+
+func getData(w htttp.ResponseWriter, r *http.Request){
+	
+}
+
+// Orm not use package
 func InsertData(w http.ResponseWriter, r *http.Request){
 
 }
 func UpdateData(w http.ResponseWriter, r *http.Request){
-	
+
+}
+func DeleteData(w http.ResponseWriter, r *http.Request){
+
 }
